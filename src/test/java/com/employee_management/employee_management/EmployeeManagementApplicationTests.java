dd01ff63091ec4947a1084242c21a8a5616ae38a
Viktor <vitygocanal@gmail.com>
2026-01-27 14:32:12 +0100

    chore: initialize Spring Boot project

---
+++ src/test/java/com/employee_management/employee_management/EmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee_management.employee_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
